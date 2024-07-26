2ed076bf735af569099c665672c10b05e14797b1
jehovah-son <chukwukasuccess0@gmail.com>
2024-07-26 01:40:05 +0100

    # new file:   assets/annie-spratt-0aF_vV0xgQk-unsplash.jpg 	# new file:   assets/fire.png 	# modified:   lib/screens/home_screen.dart 	# modified:   lib/screens/offerscreen.dart 	# modified:   pubspec.yaml

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'offerscreen.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,170 +10,180 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         color: Colors.grey,
-        padding: const EdgeInsets.symmetric(horizontal: 10 ,vertical: 80),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text(
-                    "y'ello,Ayomide",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  const Text(
-                    '0806 004 9642',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  Column(
+                    children: [
+                      const Text(
+                        "y'ello,Ayomide",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const Text(
+                        '0806 004 9642',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(
+                          right: 20,
+                          left: 10,
+                          top: 5,
+                          bottom: 5,
+                        ),
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: Colors.amber[300],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.king_bed),
+                            Text('prestige gold'),
+                            Icon(Icons.arrow_forward),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                   Container(
-                    padding: const EdgeInsets.only(
-                      right: 20,
-                      left: 10,
-                      top: 5,
-                      bottom: 5,
-                    ),
-                    width: 250,
+                    width: 100,
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                      color: Colors.amber[300],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(10)),
                     child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(Icons.king_bed),
-                        Text('prestige gold'),
-                        Icon(Icons.arrow_forward),
+                        Icon(
+                          Icons.monetization_on_rounded,
+                        ),
+                        Text(
+                          '#***',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ],
                     ),
+                  ),
+                  const Icon(
+                    Icons.notifications_none_outlined,
+                    size: 60,
                   )
                 ],
               ),
-              Container(
-                width: 100,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2),
-                    borderRadius: BorderRadius.circular(10)),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'App-only deals',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                
                   children: [
-                    Icon(
-                      Icons.monetization_on_rounded,
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder:(context) => Offerscreen() ,)
+                        );
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(50),
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.amber),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.fireplace_outlined,
+                              color: Colors.red,
+                              size: 50,
+                            ),
+                            const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Recharge offer'),
+                                Text('Get 50% bonus when you recharge with '),
+                                Text('your debit card')
+                              ],
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 20, left: 20),
+                              height: 50,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.black),
+                              child: Center(
+                                  child: Text(
+                                'View',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              )),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    Text(
-                      '#***',
-                      style: TextStyle(color: Colors.white),
+
+                    ///second item
+
+                    Container(
+                      margin: EdgeInsets.all(50),
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.amber),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.fireplace_outlined,
+                            color: Colors.red,
+                            size: 50,
+                          ),
+                          const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('Recharge offer'),
+                              Text('Get 50% bonus when you recharge with '),
+                              Text('your debit card')
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20, left: 20),
+                            height: 50,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.black),
+                            child: Center(
+                                child: Text(
+                              'View',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            )),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-              const Icon(
-                Icons.notifications_none_outlined,
-                size: 60,
-              )
-            ],
-          ),
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Text('App only'),
-         ),
-
-          Expanded(
-          
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                GestureDetector( onTap: () {
-                  
-                },
-                  child: Container(
-                  
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.amber
-                      
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.fireplace ,color: Colors.red,),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('Recharge offer'),
-                            Text('Get 50% bonus when you recharge with '),
-                            Text('your debit card')
-                          ],
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 20,left: 20),
-                          padding: EdgeInsets.all(10),
-                          height: 50,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.black
-                          ),
-                          child: Center(child: Text('View' )),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-///second item
-      Container(
-                margin: EdgeInsets.only(left: 20,right: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.amber
-                    
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.fireplace ,color: Colors.red,),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Recharge offer'),
-                          Text('Get 50% bonus when you recharge with '),
-                          Text('your debit card')
-                        ],
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 20,left: 20),
-                        padding: EdgeInsets.all(10),
-                        height: 50,
-                        width: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.black
-                        ),
-                        child: Center(child: Text('View' )),
-                      ),
-                    ],
-                  ),
-                ),
-
-              ],
-            ),
-          ),
-        ]),
+            ]),
       ),
       bottomNavigationBar: const BottomAppBar(),
     );
--- lib/screens/offerscreen.dart
+++ lib/screens/offerscreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Offerscreen extends StatefulWidget {
   const Offerscreen({super.key});
@@ -10,6 +12,108 @@ class Offerscreen extends StatefulWidget {
 class _OfferscreenState extends State<Offerscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black87,
+        body: Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 90),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Text('Hot deals (3)', style: TextStyle(color: Colors.white,fontSize: 27)),
+                  Text('checkout exciting deals made for you', style: TextStyle(color: Colors.white70,fontSize: 20),)
+                ],
+              ),
+           Container(
+            margin: EdgeInsets.all(20),
+            child: Row(children: [
+            Text('View all' ,style: TextStyle(color: Colors.amber,fontSize: 25),),
+            Icon(Icons.forward, color: Colors.amber, size: 25,)
+           ],),)
+            ],
+          ),
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                //1 container
+                Container(
+                  height: 300,
+                  width: 300,
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.all(50),
+                  decoration: BoxDecoration(
+                      color: Colors.purple[200],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'tap here to',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      Text('scratch',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ))
+                    ],
+                  ),
+                ),
+// 2 container
+                Container(
+                  height: 300,
+                  width: 300,
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.all(50),
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('tap here to',
+                          style: TextStyle(color: Colors.white, fontSize: 25)),
+                      Text(
+                        'scratch',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+// 3 container
+                Container(
+                  height: 300,
+                  width: 300,
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.all(50),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.pink[100],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'tap here to',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      Text(
+                        'scratch',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
