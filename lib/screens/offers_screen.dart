c8b48eabf26a486a36068bae2baf6548109f44d8
hawau <hawaubaraje@gmail.com>
2024-07-29 11:57:25 +0100

    modified:   lib/screens/offers_screen.dart

--- lib/screens/offers_screen.dart
+++ lib/screens/offers_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
 
 class OffersScreen extends StatelessWidget {
   const OffersScreen({super.key});
