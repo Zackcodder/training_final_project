cad739511a51b48867b44efbdf02d4e6fccf478b
hawau <hawaubaraje@gmail.com>
2024-08-06 13:57:11 +0100

    new file:   lib/screens/bundlescreen.dart 	modified:   lib/screens/home_screen.dart

---
+++ lib/screens/bundlescreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bundlescreen extends StatelessWidget {
+  const Bundlescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Buy bundles'),
+      ),
+ 
+    );
+  }
+}
