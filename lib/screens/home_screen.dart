7cdcd72950ebd36fd2a19b74cea57554c59afcb4
hawau <hawaubaraje@gmail.com>
2024-08-20 12:38:39 +0100

    modified:   lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:training_final_project/screens/bundlescreen.dart';
 import 'package:training_final_project/screens/borrow%20screen.dart';
 import 'package:training_final_project/screens/mega_deal_screen.dart';
 import 'package:training_final_project/screens/offerscreen.dart';
